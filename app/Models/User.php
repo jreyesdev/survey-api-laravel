66b1739fa7a44948dd6d2ce8ca8d8e6effb69731
Jesus Reyes <djrm1204@gmail.com>
2022-05-08 00:09:57 -0400

    Token y Custom cast al modelo User

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,13 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Str;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -37,4 +40,45 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    //CUSTOM CASTS
+    /**
+     * The name attribute
+     * @return Attribute
+     */
+    public function name(): Attribute
+    {
+        return new Attribute(
+            // Get
+            fn ($value) => Str::title($value),
+            // Set
+            fn ($value) => Str::lower($value),
+        );
+    }
+    /**
+     * The email attribute
+     * @return Attribute
+     */
+    public function email(): Attribute
+    {
+        return new Attribute(
+            // Get
+            fn ($value) => Str::lower($value),
+            // Set
+            fn ($value) => Str::lower($value),
+        );
+    }
+    /**
+     * The password attribute
+     * @return Attribute
+     */
+    public function password(): Attribute
+    {
+        return new Attribute(
+            // Get
+            fn ($value) => $value,
+            // Set
+            fn ($value) => bcrypt($value),
+        );
+    }
 }
