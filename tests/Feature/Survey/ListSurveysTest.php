32bc079d4c3bd45090fe58f2b272909a2e820fe8
Jesus Reyes <djrm1204@gmail.com>
2022-09-18 18:00:05 -0400

    Agregado tres test a ListSurveysTest

--- tests/Feature/Survey/ListSurveysTest.php
+++ tests/Feature/Survey/ListSurveysTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\Survey;
 use App\Models\Survey;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Collection;
 use Tests\AuthUser;
 use Tests\TestCase;
 
@@ -12,14 +13,24 @@ class ListSurveysTest extends TestCase
 {
     use RefreshDatabase;
 
+    /** Crea encuestas al usuario segun id
+     * @param int
+     * @param int
+     * @return Collection
+     */
+    private function createSurveys(int $id, int $count = 3): Collection
+    {
+        return Survey::factory($count)->create([
+            'user_id' => $id,
+        ]);
+    }
+
     /** @test */
     public function list_all_surveys_auth_user()
     {
-        $user = AuthUser::createAuthUser();
+        [$user, $token] = AuthUser::createAuthUser();
 
-        $surveys = Survey::factory(3)->create([
-            'user_id' => $user[0]->id,
-        ]);
+        $surveys = $this->createSurveys($user->id);
 
         $surveys = $surveys->map(function ($item) {
             return [
@@ -34,11 +45,81 @@ class ListSurveysTest extends TestCase
         });
 
         $response = $this->getJson('api/surveys', [
-            'Authorization' => 'Bearer ' . $user[1]
+            'Authorization' => 'Bearer ' . $token
         ]);
 
         $response->assertStatus(200)->assertJson([
             'data' => $surveys->toArray()
         ]);
+    }
+
+    /** @test */
+    public function user_only_sees_their_survery()
+    {
+        [$user, $token] = AuthUser::createAuthUser();
+        [$user2] = AuthUser::createAuthUser();
+
+        $surveys = $this->createSurveys($user->id);
+        $this->createSurveys($user2->id, 5);
+
+        $surveys = $surveys->map(function ($item) {
+            return [
+                'id' => $item['id'],
+                'title' => $item['title'],
+                'slug' => $item['slug'],
+                'status' => $item['status'] !== 'draft',
+                'description' => $item['description'],
+                'expire_date' => $item['expire_date'],
+                'questions' => [],
+            ];
+        });
+
+        $response = $this->getJson('api/surveys/' . $surveys[0]['id'], [
+            'Authorization' => 'Bearer ' . $token
+        ]);
+
+        $response->assertStatus(200)->assertJson($surveys[0]);
+    }
+
+    /** @test */
+    public function user_should_not_see_a_survey_of_other_user()
+    {
+        [$user, $token] = AuthUser::createAuthUser();
+        [$user2] = AuthUser::createAuthUser();
+
+        $this->createSurveys($user->id);
+        $surveys = $this->createSurveys($user2->id, 5);
+
+        $surveys = $surveys->map(function ($item) {
+            return [
+                'id' => $item['id'],
+                'title' => $item['title'],
+                'slug' => $item['slug'],
+                'status' => $item['status'] !== 'draft',
+                'description' => $item['description'],
+                'expire_date' => $item['expire_date'],
+                'questions' => [],
+            ];
+        });
+
+        $response = $this->getJson('api/surveys/' . $surveys[0]['id'], [
+            'Authorization' => 'Bearer ' . $token
+        ]);
+
+        $response->assertStatus(403);
+    }
+
+    /** @test */
+    public function user_should_not_see_a_survey_that_does_not_exist()
+    {
+        [$user, $token] = AuthUser::createAuthUser();
+
+        $this->createSurveys($user->id);
+
+        $response = $this->getJson('api/surveys/10', [
+            'Authorization' => 'Bearer ' . $token
+        ]);
+
+        $response->assertStatus(404);
     }
 }
