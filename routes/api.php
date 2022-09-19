3a02a64efd7bbc420ef6c411774259c4fb442d37
Jesus Reyes <djrm1204@gmail.com>
2022-09-19 00:47:08 -0400

    Rutas, Controlador y resource de encuestas

--- routes/api.php
+++ routes/api.php
@@ -1,25 +1,18 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\SurveyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
     Route::post('logout', [AuthController::class, 'logout']);
+
+    //SURVEY
+    Route::resource('surveys', SurveyController::class);
 });
 
 Route::post('register', [AuthController::class, 'register']);
